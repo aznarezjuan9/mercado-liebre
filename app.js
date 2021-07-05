b1bbda08ce6b0ec3188acd2152506f85e197a83e
aznarezjuan9 <juan.aznarez@hotmail.com>
2021-07-05 18:47:13 -0300

    agrego configuracion heroku

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const path = require('path');
 
-app.listen(3000, function(){ 
-    console.log("Servidor corriendo")
+app.listen(process.env.PORT || 3000, function(){ 
+    console.log("Servidor corriendo en el puerto 3000")
 });
 
  app.get("/", function(req, res){
